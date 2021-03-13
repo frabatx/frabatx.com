1c7aab81af891cfc8d8cb9ebb78d22ae591a2026
frabatx <francesco92battista@gmail.com>
2021-03-13 03:00:14 +0100

    Inizializzato welcome iniziale, TODO Button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-   console.log(navLinks); 
+    const nav = document.querySelector('.navbar');
+    const navLinks = document.querySelectorAll('.nav-link');
+    console.log(navLinks); 
+    console.log(burger); 
+    console.log(nav); 
     burger.addEventListener('click', () => {
         // Toggle Nav
         nav.classList.toggle('nav-active');
@@ -11,7 +13,7 @@ const navSlide = () => {
             if(link.style.animation){
                 link.style.animation = '';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 1}s`; 
+                link.style.animation = `navLinkFade 0.3s ease forwards ${index /7 + 1}s`; 
             }
         });
 
