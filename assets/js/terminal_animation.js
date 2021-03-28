efdfe40d49e14e059a91393f2875fcd021b54325
frabatx <francesco92battista@gmail.com>
2021-03-28 20:03:30 +0200

    Modifiche di stile, aggiunta opzione skip animation

--- assets/js/terminal_animation.js
+++ assets/js/terminal_animation.js
@@ -9,24 +9,23 @@ const terza = "I don’t want to waste too much of your time, so I’m gonna let
 const quarta = "Have fun!"
 
 
-// Animazione cursore
+// ANIMAZIONE CURSORE
 let cursor = gsap.to('#cursor', {opacity: 0, ease: "power2.inOut", repeat: -1})
 
-// Animazione terminale
-let terminalTl = gsap.timeline();
+// ANIMAZIONE TERMINALE
+let terminalTl = gsap.timeline().addLabel("animazione-terminale");
 
 terminalTl.to('.wrapper', {duration:0.5, width: "70vw", delay: 0.5, ease: "power4.inOut"})
            .to('.wrapper', {duration:0.5, height: "70vh", delay: 0.5, ease: "power4.inOut"})
            .from('#codename', {duration: 1, x: "-30vw" , ease: "power3.out"})
            .set('#cursor', {visibility: "visible"})
 
-// Animazione Hi
-let firstSentenceTl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 3});
+// ANIMAZIONE HI, I'M
+let firstSentenceTl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 3}).addLabel("animazione-hi");
 firstSentenceTl.to('#im', {duration: 1,text:firstSentence,onComplete: ()=>secondSentenceTl.play()})
 
-// Animazione francesco
-let secondSentenceTl = gsap.timeline().pause();
-// Aggiungo al target le words da inserire
+// ANIMAZIONE FRANCESCO FRABATX
+let secondSentenceTl = gsap.timeline().addLabel("animazione-frabatx").pause();
 
 secondSentence.forEach(word =>{
   let tl = gsap.timeline({repeat: 1, yoyo: true , repeatDelay:.5});
@@ -34,47 +33,47 @@ secondSentence.forEach(word =>{
   secondSentenceTl.add(tl);
 })
 
-//Animazione introduzione
-let introduceEditorTl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1})
-introduceEditorTl.to('#im', {duration: 2, 
-                             text:introduceEditor
-                            })
+// ANIMAZIONE INTRODUZIONE
+let introduceEditorTl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1}).addLabel("animazione-introduzione");
+introduceEditorTl.to('#im', {duration: 2, text:introduceEditor})
 
-// Animazione apertura vim
-let vimTl = gsap.timeline()
+// ANIMAZIONE VIM
+let vimTl = gsap.timeline().addLabel("animazione-vim");
 vimTl.to('#im', {duration: 1.5, 
                  text: "vim presentation.txt"})
 vimTl.addPause(">1", clearEditor, [true])
 
-// Animazione text in vim
+// ANIMAZIONE TEXT IN VIM
 let primaTl = gsap.timeline();
 let secondaTl = gsap.timeline();
 let terzaTl = gsap.timeline();
 let quartaTl = gsap.timeline();
 let lastSentenceTl = gsap.timeline();
 
-primaTl.to('#prima', {duration: 3, text:prima, onComplete: removeSpanRows, onCompleteParams: [1]})
-secondaTl.to('#seconda', {duration: 3, text:seconda, onComplete: removeSpanRows, onCompleteParams: [1]})
-terzaTl.to('#terza', {duration: 5, text:terza, onComplete: removeSpanRows, onCompleteParams: [2]})
-quartaTl.to('#quarta', {duration: 2, text:quarta, onComplete: removeSpanRows, onCompleteParams: [1]})
+primaTl.to('#prima', {duration: 3, text:prima, onComplete: removeSpanRows, onCompleteParams: [2]})
+secondaTl.to('#seconda', {duration: 3, text:seconda, onComplete: removeSpanRows, onCompleteParams: [2]})
+terzaTl.to('#terza', {duration: 5, text:terza, onComplete: removeSpanRows, onCompleteParams: [3]})
+quartaTl.to('#quarta', {duration: 2, text:quarta, onComplete: removeSpanRows, onCompleteParams: [2]})
 quartaTl.addPause(">2", clearEditor, [false])
 
-// Animazione lancio server
+// ANIMAZINOE HUGO SERVER
 lastSentenceTl.to('#target', {duration: 1.5, text: "hugo server"})
 lastSentenceTl.addPause(">2");
 
-// Animazione terminale chiusura
+// ANIMAZIONE CHIUSURA TERMINALE
 let terminalFineTl = gsap.timeline();
 
-terminalFineTl.set('#codename, #cursor, #target', {duration:1,visibility: "hidden"})
+terminalFineTl.set('#codename, #cursor, #target,#im, #prima, #seconda,#terza,#quarta', {duration:1,visibility: "hidden"})
               .to('.wrapper', {duration:0.5, width: "1vw", delay: 0.5, ease: "power4.inOut"})
               .to('.wrapper', {duration:0.5, height: "0", delay: 0.5, ease: "power4.inOut"})
-              .set('.terminal-container', {display: "none"})
-              .set('.wrapper', {display: "none"})
-              .set('.main', {duration: 1, visibility: "visible", display: "block"})
+              .set('#btn-skip',{display: "none"})
+              .set('.container-animation', {duration: 1, opacity: 0})
+              .set('.container-animation', {display: "none"})
+              // .set('.wrapper', {display: "none"})
+              .to('.main', {duration: 3, opacity: 1, display: "block"})
            
 
-// Creo una master Timeline che aggancia tutte le sequenze una dopo l'altra
+// MASTER TIMELINE
 let masterTl = gsap.timeline()
 masterTl.add(terminalTl)
         .add(firstSentenceTl)
@@ -85,38 +84,45 @@ masterTl.add(terminalTl)
         .add(terzaTl)
         .add(quartaTl)
         .add(lastSentenceTl)
+        .add("last")
         .add(terminalFineTl)
 
 
-// Funzioni addizionali
+// ADDICTIONAL FUNCTIONS
 function removeSpanRows(loop){
-    var x = document.getElementById('rows');
-    for(i = 0; i < (loop*2); i++){
-      x.children[0].remove();
-    }
+  var x = document.getElementById('rows');
+  for(i = 0; i < (loop*2); i++){
+    x.children[0].remove();
   }
-  function clearEditor(rows){
-    let string = '';
-    $('#codename').empty()
-    $('#im').empty()
-    $('#prima').empty()
-    $('#seconda').empty()
-    $('#terza').empty()
-    $('#quarta').empty()
-    if(rows){
-        var element = document.getElementById('background-layer');
-        var divHeight = element.offsetHeight;
-        var lineHeight = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue("line-height"));
-        var lines = divHeight / lineHeight;
-        for(i =0 ; i< (lines-5); i++){
-            string += '<span>~</span><br>'
-        }
-        $('#rows').html(string)
-    
-    }else{
-        $('#rows').empty() 
-        $('#codename').html("[frabatx@admin]$: >") 
-    }
+}
+function clearEditor(rows){
+  let string = '';
+  $('#codename').empty()
+  $('#im').empty()
+  $('#prima').empty()
+  $('#seconda').empty()
+  $('#target').empty()
+  $('#terza').empty()
+  $('#quarta').empty()
+  if(rows){
+      var element = document.getElementById('background-layer');
+      var divHeight = element.offsetHeight;
+      var lineHeight = parseInt(document.defaultView.getComputedStyle(element, null).getPropertyValue("line-height"));
+      var lines = divHeight / lineHeight;
+      for(i =0 ; i< (lines-5); i++){
+          string += '<span>~</span><br>'
+      }
+      $('#rows').html(string)
+  
+  }else{
+      $('#rows').empty() 
+      $('#codename').html("[frabatx@admin]$: >") 
   }
+}
 
-
+// SKIP ANIMATION
+$("#btn-skip").on('click', ()=>{
+  console.log('skippo');
+  clearEditor(false);
+  masterTl.seek("last");
+})
